f8f06957b58222e00187cabb76d5b8c287385ab1
Sergey Sharybin <sergey.vfx@gmail.com>
2024-04-21 18:20:43 +0200

    Beginning of the cocktail manager

---
+++ source/cocktail_manager.lua
@@ -0,0 +1,6 @@
+
+COCKTAILS = {
+  { name = "Flaming Garlic",      rune_composition = {1, 0, 0}, color=0.1, viscosity=0.2, img = gfx.image.new('images/spider_cocktail_sheet') },
+  { name = "Salty Spider Shroom", rune_composition = {0, 1, 2}, color=0.2, viscosity=0.8, img = gfx.image.new('images/spider_cocktail_sheet') },
+  { name = "Smelly Cat Parfume",  rune_composition = {0, 1, 2}, color=0.9, viscosity=0.4, img = gfx.image.new('images/spider_cocktail_sheet') },
+}
