6992e9b624f5b8a946d86af17bb88e0e3aef16ec
Sergey Sharybin <sergey.vfx@gmail.com>
2024-04-23 01:30:56 +0200

    Use smaller sound file which fits into the device memory

--- source/sound.lua
+++ source/sound.lua
@@ -1,5 +1,7 @@
 local sp <const> = playdate.sound.sampleplayer
 
+local bg_loop_menu = sp.new("sound/menu_music_loop_thin")
+
 SOUND = {
   frog_croak = sp.new("sound/sound_sample"),
   fire_blow = sp.new("sound/fire_blow"),
@@ -7,8 +9,8 @@ SOUND = {
   drop_01 = sp.new("sound/drop_01"),
   drop_02 = sp.new("sound/drop_02"),
   drop_03 = sp.new("sound/drop_03"),
-  bg_loop_menu = sp.new("sound/menu_music_loop"),
-  bg_loop_gameplay = sp.new("sound/menu_music_loop"),
+  bg_loop_menu = bg_loop_menu,
+  bg_loop_gameplay = bg_loop_menu,
   menu_confirm = sp.new("sound/menu_item_confirm"),
   menu_highlight = sp.new("sound/menu_item_highlight"),
   stir_sound = sp.new("sound/pot_stir")
