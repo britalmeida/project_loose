92535a7ba579450b9d4eeaed1d835ed137738b65
Inês Almeida <britalmeida@gmail.com>
2024-04-23 00:16:15 +0200

    Sound: low key fireplace sounds

--- source/sound.lua
+++ source/sound.lua
@@ -1,9 +1,9 @@
 local sp <const> = playdate.sound.sampleplayer
 
 SOUND = {
-  cat_meow = sp.new("sound/sound_sample"),
-  bg_loop_menu = sp.new("sound/sound_sample"),
-  bg_loop_gameplay = sp.new("sound/sound_sample"),
+  frog_croak = sp.new("sound/sound_sample"),
+  bg_loop_menu = sp.new("sound/fire_burn"),
+  bg_loop_gameplay = sp.new("sound/fire_burn"),
   menu_confirm = sp.new("sound/menu_item_confirm"),
   menu_highlight = sp.new("sound/menu_item_highlight"),
 }
