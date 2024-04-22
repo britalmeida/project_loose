d01157ce7a2f8c815005dfe167ad349c2f4f582a
Simon Thommes <simon.thommes@gmx.de>
2024-04-22 02:26:37 +0200

    Adjust target potions

--- source/cocktail_manager.lua
+++ source/cocktail_manager.lua
@@ -1,8 +1,8 @@
 local gfxi <const> = playdate.graphics.image
 
 COCKTAILS = {
-  { name = "Flarlick",  rune_composition = {1, 0, 0}, color=0.1, viscosity=0.2, img = gfxi.new('images/spider_cocktail_sheet') },
-  { name = "Spishroom", rune_composition = {0, 1, 2}, color=0.2, viscosity=0.8, img = gfxi.new('images/spider_cocktail_sheet') },
+  { name = "Flarlick",  rune_composition = {4, 1, 0}, color=0.1, viscosity=0.2, img = gfxi.new('images/snailiva_cocktail_sheet') },
+  { name = "Spishroom", rune_composition = {0, 3, 7}, color=0.2, viscosity=0.8, img = gfxi.new('images/greentoe_cocktail_sheet') },
   { name = "Catfume",   rune_composition = {0, 1, 2}, color=0.9, viscosity=0.4, img = gfxi.new('images/spider_cocktail_sheet') },
 }
 
