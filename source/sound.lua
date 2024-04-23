f62fecf1e2f6287b313596d6a3f2c60cda12ede3
Inês Almeida <britalmeida@gmail.com>
2024-04-23 22:23:38 +0200

    SFX: stop stirring sounds when the crank stops + cleanup sound files
    Removed sounds that are not needed as that affects the pdx file size and deploy times

--- source/sound.lua
+++ source/sound.lua
@@ -3,15 +3,13 @@ local sp <const> = playdate.sound.sampleplayer
 local bg_loop_menu = sp.new("sound/menu_music_loop_thin")
 
 SOUND = {
-  frog_croak = sp.new("sound/sound_sample"),
   fire_blow = sp.new("sound/fire_blow"),
   fire_burn = sp.new("sound/fire_burn"),
   drop_01 = sp.new("sound/drop_01"),
   drop_02 = sp.new("sound/drop_02"),
   drop_03 = sp.new("sound/drop_03"),
+  stir_sound = sp.new("sound/pot_stir"),
   bg_loop_menu = bg_loop_menu,
   bg_loop_gameplay = bg_loop_menu,
   menu_confirm = sp.new("sound/menu_item_confirm"),
-  menu_highlight = sp.new("sound/menu_item_highlight"),
-  stir_sound = sp.new("sound/pot_stir")
 }
