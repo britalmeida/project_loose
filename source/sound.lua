9b1a671582220c69b5c5d7081651e956e582e7df
Inês Almeida <britalmeida@gmail.com>
2024-04-24 02:12:38 +0200

    Music: hook up menu vs gameplay music loops

--- source/sound.lua
+++ source/sound.lua
@@ -1,7 +1,5 @@
 local sp <const> = playdate.sound.sampleplayer
 
-local bg_loop_menu = sp.new("sound/menu_music_loop_thin")
-
 SOUND = {
   fire_blow = sp.new("sound/fire_blow"),
   fire_burn = sp.new("sound/fire_burn"),
@@ -9,7 +7,7 @@ SOUND = {
   drop_02 = sp.new("sound/drop_02"),
   drop_03 = sp.new("sound/drop_03"),
   stir_sound = sp.new("sound/pot_stir"),
-  bg_loop_menu = bg_loop_menu,
-  bg_loop_gameplay = bg_loop_menu,
+  bg_loop_menu = sp.new("sound/menu_music_loop"),
+  bg_loop_gameplay = sp.new("sound/gameplay_music_loop"),
   menu_confirm = sp.new("sound/menu_item_confirm"),
 }
