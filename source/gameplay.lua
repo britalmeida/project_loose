095e0f531ec2ed50f224597dd022eb7306a3cba9
Inês Almeida <britalmeida@gmail.com>
2024-04-25 21:22:31 +0200

    Sound: fix background burn sound not playing and fire blow sound not stopping

--- source/gameplay.lua
+++ source/gameplay.lua
@@ -290,8 +290,8 @@ function Tick_gameplay()
             end
         end
     else
-        if SOUND.fire_burn:isPlaying() then
-            SOUND.fire_burn:stop()
+        if SOUND.fire_blow:isPlaying() then
+            SOUND.fire_blow:stop()
         end
         GAMEPLAY_STATE.flame_amount = 0
     end
@@ -302,6 +302,16 @@ function Tick_gameplay()
     else
         -- The flame heats up the cauldron
         GAMEPLAY_STATE.heat_amount += 0.01 * GAMEPLAY_STATE.flame_amount
+    end
+    print(GAMEPLAY_STATE.heat_amount)
+    if GAMEPLAY_STATE.heat_amount > 0.1 then
+        if not SOUND.fire_burn:isPlaying() then
+            SOUND.fire_burn:play()
+        end
+    else
+        if SOUND.fire_burn:isPlaying() then
+            SOUND.fire_burn:stop()
+        end
     end
 
     -- Update liquid color
