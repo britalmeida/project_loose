21a29db1f20d7b2fc3f35a5dd0978fc108b7c823
Falk David <falk@blender.org>
2024-05-04 19:56:22 +0200

    Don't use getFPS() in Ingredient:hover()

--- source/ingredients.lua
+++ source/ingredients.lua
@@ -126,7 +126,7 @@ function Ingredient:hover()
   if self.hover_tick > 0 then
     -- Move sprite to the front
     self:setZIndex(Z_DEPTH.grabbed_ingredient)
-    local time = GAMEPLAY_STATE.game_tick / playdate.getFPS()
+    local time = playdate.getElapsedTime()
     local wiggle_freq = 1
     local x_offset = math.sin(time * 2 * math.pi * (wiggle_freq - 0.1))
     local y_offset = math.sin(time * 2 * math.pi * (wiggle_freq + 0.1))
