f936a7444138050ba764c10c74c83831914a97de
JulienKaspar <julien.kaspar@googlemail.com>
2024-05-05 12:45:33 +0200

    Balanced cocktail more
    Overdose was on the correct color from the start

--- source/cocktail_manager.lua
+++ source/cocktail_manager.lua
@@ -5,7 +5,7 @@ COCKTAILS = {
   { name="Silkini",  rune_composition={1, 2, 0}, color=0.0, img=gfxi.new('images/cocktails/silkini_sheet') }, -- Intro 2 (Love & Doom)
   { name="Green Toe",  rune_composition={0, 3, 9}, color=0.25, img=gfxi.new('images/cocktails/green_toe_sheet') }, --Easy (Doom & Weeds but specific)
   { name="Hodge Podge",   rune_composition={8, 2, 6}, color=0.8, img=gfxi.new('images/cocktails/hodge_podge_sheet') }, --Medium (3 Runes. Less clear art)
-  { name="Overdose",  rune_composition={1, 6, 3}, color=0.6, img=gfxi.new('images/cocktails/overdose_sheet') }, --Hard (3 runes and tricky combination)
+  { name="Overdose",  rune_composition={1, 6, 3}, color=0.15, img=gfxi.new('images/cocktails/overdose_sheet') }, --Hard (3 runes and tricky combination)
   { name="Dicybrew",  rune_composition={1, 1, 1}, color=1,0, img=gfxi.new('images/cocktails/dicey_brew_sheet') }, --Random
 }
 
