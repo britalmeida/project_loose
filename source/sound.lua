7c2364d3ab887de87411c10395fd8ef082185b9d
Inês Almeida <britalmeida@gmail.com>
2024-05-10 21:06:29 +0200

    Sound: test streaming long sounds

--- source/sound.lua
+++ source/sound.lua
@@ -1,13 +1,14 @@
 local sp <const> = playdate.sound.sampleplayer
+local fp <const> = playdate.sound.fileplayer
 
 SOUND = {
   fire_blow = sp.new("sound/fire_blow"),
-  fire_burn = sp.new("sound/fire_burn"),
+  fire_burn = fp.new("sound/fire_burn"),
   drop_01 = sp.new("sound/drop_01"),
   drop_02 = sp.new("sound/drop_02"),
   drop_03 = sp.new("sound/drop_03"),
   stir_sound = sp.new("sound/pot_stir"),
-  bg_loop_menu = sp.new("sound/menu_music_loop"),
-  bg_loop_gameplay = sp.new("sound/gameplay_music_loop"),
+  bg_loop_menu = fp.new("sound/menu_music_loop"),
+  bg_loop_gameplay = fp.new("sound/gameplay_music_loop"),
   menu_confirm = sp.new("sound/menu_item_confirm"),
 }
