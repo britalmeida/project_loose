a3b7a86b54da1b3a394d9d2aed54e3082fb81898
Inês Almeida <britalmeida@gmail.com>
2024-05-12 19:48:32 +0200

    Put the instructions prompt in its rightful place

--- source/visuals.lua
+++ source/visuals.lua
@@ -639,7 +639,7 @@ local function draw_ui_prompts()
     end
 
     gfx.pushContext()
-        TEXTURES.instructions_prompt:draw(0, 0)
+        TEXTURES.instructions_prompt:draw(0, 240-TEXTURES.instructions_prompt.height, 0)
         TEXTURES.b_prompt:drawImage(1, 362, 203)
     gfx.popContext()
 end
