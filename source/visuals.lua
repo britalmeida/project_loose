fc6a12680213f325a1bf11e851e4fd7aa3ecf7e2
JulienKaspar <julien.kaspar@googlemail.com>
2024-05-19 00:57:37 +0200

    Fix for runes moving by themselves too fast
    Simple fix. Bigger cleanup would be better.

--- source/visuals.lua
+++ source/visuals.lua
@@ -125,7 +125,7 @@ function add_rune_travel_anim()
 
     -- add current rune count and new anim to table
     local rune_count = shallow_copy(GAMEPLAY_STATE.rune_count)
-    table.insert(rune_anim_table, {rune_count, animator.new(3*1000, 0.0, 1.0, inOutQuad), animator.new(30*1000, 0.0, 1.0, inOutQuad)})
+    table.insert(rune_anim_table, {rune_count, animator.new(3*1000, 0.0, 1.0, inOutQuad), animator.new(200*1000, 0.0, 1.0, inOutQuad)})
 
 end
 
