b91ab9aa1218dc08fee1cef74aa089160b21a303
JulienKaspar <julien.kaspar@googlemail.com>
2024-07-10 11:05:35 +0200

    Nudge stir completion a bit later
    Now that we have the bubbling and puff, this is only needed as a subtle push,
    but should definitely happen close to ending manual stirring.

--- source/gameplay.lua
+++ source/gameplay.lua
@@ -833,7 +833,7 @@ function update_liquid()
     STIR_CHANGE = Clamp(stir_change_unclamped, min_stir_change, max_stir_change)
 
     -- Calculate current stirring effect.
-    if floating_drops > 0 and STIR_FACTOR > 0.8 then
+    if floating_drops > 0 and STIR_FACTOR > 0.9 then
         STIR_FACTOR += STIR_FACTOR * 0.002
         STIR_FACTOR = math.min(STIR_FACTOR, 1)
     elseif floating_drops == 0 then
