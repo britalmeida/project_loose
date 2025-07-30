britalmeida/project_loose

commit a573cbf0055604c76848b1c6b83f4ef28d85b878

Fix frog not licking his eyeballs when he should - significant changes
Repro: start snailiva, add perfume drops until heart glows.
-> Frog does nothing until something else happens e.g. after he speaks.

Logic for Froggo:Lick_eyeballs() was not being run because it was dependant on CHECK_IF_DELICIOUS
That variable has complex logic to be on and I see no reason to not always check if the frog should react to the cocktail being good.

Removed CHECK_IF_DELICIOUS.
Fallout: adjusting the logic and fixing the eyeball licking from not changing the frog's idle state
could have all kinds of unpredictable side effects due to the complex conditional logic with timers for the frog and gameplay.
I couldn't do a simple fix, had to re-write quite a bit for it to make sense and can't speak for side effects.

source/tutorial_frog.lua
@@ -284,7 +284,9 @@ function Froggo:Click_the_frog()
     -- Make it a bit smaller, so we don't accidentally click on the frog.
     bounds:inset(15, 15)
     if bounds:containsPoint(GAMEPLAY_STATE.cursor_pos) and self.state == ACTION_STATE.idle then
-        if Is_potion_good_enough() then
+        if Is_potion_good_enough() and self.anim_current == self.anim_eyeball then
+            -- Trigger cocktail drinking same as if pressing B
+            -- if the game can be finished and the frog is animating.
             local automated = false
             self:Ask_the_frog(automated)
         else
@@ -311,29 +313,30 @@ function Froggo:Notify_the_frog()
     end
 end
 
-function Froggo:Lick_eyeballs()
+function Froggo:Check_if_delicious()
+    -- Frog only reacts to the potion being correct after finishing what he was doing before.
     if self.state == ACTION_STATE.idle then
-        -- Check if the rune count is still within the goal
-        local rune_count_unchanged = true
-        for i in pairs(GAMEPLAY_STATE.rune_count) do
-            local distance_from_goal = math.abs(GAMEPLAY_STATE.rune_count[i] - GAMEPLAY_STATE.rune_count_unstirred[i])
-            if distance_from_goal > GOAL_TOLERANCE then
-                rune_count_unchanged = false
-            end
-        end
-
         -- lick eyeballs if:
         -- - The potion is confirmed to be good
         -- - The potion was good already, some new ingredients were thrown in but it should still be the same
-        if (Is_potion_good_enough() and CHECK_IF_DELICIOUS) or
-        (Are_ingredients_good_enough() and CHECK_IF_DELICIOUS and rune_count_unchanged) then
-            self.sound_state = SOUND_STATE.eyelick
-            self:set_frog_sounds()
-            self:flash_b_prompt(60*1000)
-            self:start_animation(self.anim_eyeball)
-            self.x_offset = -11
-            self:start_thought_bubble()
-            CHECK_IF_DELICIOUS = false
+        if Are_ingredients_good_enough() then -- the runes are on target.
+            if (GAMEPLAY_STATE.dropped_ingredients == 0 or not Do_floating_ingredients_affect_goodness()) then
+                self.state = ACTION_STATE.reacting
+                self.sound_state = SOUND_STATE.eyelick
+                self:set_frog_sounds()
+                self:flash_b_prompt(self.anim_eyeball.duration)
+                self:start_animation(self.anim_eyeball)
+                self.x_offset = -11
+                self:start_thought_bubble()
+            end
+        end
+    elseif self.state == ACTION_STATE.reacting and self.anim_current == self.anim_eyeball then
+        -- Stop licking eyeballs if the potion stopped being correct.
+        if not Are_ingredients_good_enough() then
+            ANIMATIONS.b_prompt.paused = true
+            ANIMATIONS.b_prompt.frame = 1
+            self:stop_thought_bubble()
+            self:go_idle()
         end
     end
 end

source/gameplay.lua
@@ -92,8 +92,6 @@ TREND = 0
 -- If a positive reinforcement can be triggered
 CAN_REINFORCE = false
 PREV_RUNE_COUNT = {0, 0, 0}
--- fog will check again if win conditions are correct
-CHECK_IF_DELICIOUS = false
 
 PLAYER_LEARNED = {
     how_to_grab = false,
@@ -163,13 +161,9 @@ GAMEPLAY_TIMERS = {
         end),
     speech_timer = playdate.timer.new(100, function()
         FROG:stop_speech_bubble()
-        CHECK_IF_DELICIOUS = true
         end),
     frog_go_idle = playdate.timer.new(100, function()
         FROG:go_idle()
-        -- Check if the potion is still good. If yes, start eyeball lick anim
-        CHECK_IF_DELICIOUS = true
-        FROG:Lick_eyeballs()
         end),
     frog_give_hint_fire = playdate.timer.new(100, function()
         PLAYER_STRUGGLES.too_much_fire = true
@@ -1088,7 +1082,6 @@ function update_liquid()
             table.shallowcopy(GAMEPLAY_STATE.rune_count),
             1
         }}
-        CHECK_IF_DELICIOUS = true
         if not GAMEPLAY_STATE.stirring_complete and GAMEPLAY_STATE.counting_stirs then
             GAMEPLAY_STATE.stirring_complete = true
             GAMEPLAY_STATE.puff_anim_started = false
@@ -1126,6 +1119,18 @@ end
 
 -- Game target goal checks
 
+function Do_floating_ingredients_affect_goodness()
+    -- Check if the rune count is still within the goal
+    for i in pairs(GAMEPLAY_STATE.rune_count) do
+        if TARGET_COCKTAIL.rune_count[i] ~= 0 then
+            local distance_from_goal = math.abs(GAMEPLAY_STATE.rune_count[i] - GAMEPLAY_STATE.rune_count_unstirred[i])
+            if distance_from_goal > GOAL_TOLERANCE then
+                return true
+            end
+        end
+    end
+    return false
+end
 
 function Are_ingredients_good_enough()
     for i=1, NUM_RUNES do
@@ -1168,13 +1173,10 @@ function Calculate_goodness()
     if not RECIPE_STRUGGLE_STEPS then
         if math.abs(TREND - prev_trend) == 2 or math.abs(diff_change_overall) > 0.01 then
             FROG:Notify_the_frog()
-            -- Stop potential blinking from eyeball lick
-            ANIMATIONS.b_prompt.frame = 1
-            ANIMATIONS.b_prompt.paused = true
         end
-    elseif CHECK_IF_DELICIOUS then
-        FROG:Lick_eyeballs()
     end
+
+    FROG:Check_if_delicious()
 end
 
 

commit b5afdd7690db6be65f2045431dd11a51c6aed49c

Fix: frog should facepalm/headshake when the potion stops being ready
This got broken in a573cbf when the eyeball licking didn't keep the Frog's idle state.
I added the disbelief animation because the transition felt lacking without noticing it was supposed to facepalm.
Now it does again and the disbelief animation is no longer needed.

source/tutorial_frog.lua
@@ -184,7 +184,6 @@ function Froggo:init()
     self.anim_blabla        = animloop.new(8 * frame_ms, gfxit.new('images/frog/animation-blabla'), true)
     self.anim_tickleface    = animloop.new(2.5 * frame_ms, gfxit.new('images/frog/animation-tickleface'), false)
     self.anim_eyeball       = animloop.new(4 * frame_ms, gfxit.new('images/frog/animation-eyeball'), true)
-    self.anim_disbelief     = animloop.new(4 * frame_ms, gfxit.new('images/frog/animation-disbelief'), false)
     self.anim_frogfire      = animloop.new(4 * frame_ms, gfxit.new('images/frog/animation-frogfire'), true)
     self.anim_facepalm      = animloop.new(4 * frame_ms, gfxit.new('images/frog/animation-facepalm'), true)
     self.anim_urgent        = animloop.new(3.75 * frame_ms, gfxit.new('images/frog/animation-urgent'), true)
@@ -316,15 +315,8 @@ function Froggo:Notify_of_deliciousness_change()
     end
 
     if self.state == ACTION_STATE.idle then
-        -- If the potion was right already, give proper reaction :D
-        if self.anim_current == self.anim_eyeball then
-            if TUTORIAL_COMPLETED then
-                self:facepalm()
-            else
-                self:do_headshake()
-            end
-        -- Otherwise react to ingredient direction
-        elseif TREND > 0 and TUTORIAL_COMPLETED then
+        -- React to ingredient direction
+        if TREND > 0 and TUTORIAL_COMPLETED then
             self:do_happy_look()
         elseif TREND < 0 and TUTORIAL_COMPLETED then
             self:do_headshake()
@@ -356,9 +348,11 @@ function Froggo:Check_if_delicious()
             ANIMATIONS.b_prompt.paused = true
             ANIMATIONS.b_prompt.frame = 1
             self:stop_thought_bubble()
-            self:start_animation(self.anim_disbelief)
-            self:prepare_to_idle(self.anim_disbelief.delay * self.anim_disbelief.endFrame)
-            self:start_sound(SOUND_STATE.silent)
+            if TUTORIAL_COMPLETED then
+                self:facepalm()
+            else
+                self:do_headshake()
+            end
         end
     end
 end